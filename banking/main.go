5ad3b5d0c0d3894e09de59068902c06525858a2c
Thiago Medeiros <si-thiago@hotmail.com>
2021-02-16 23:25:31 -0300

    Handling Content-Type request - I can check which Content-Type was requested and send the   response in the right type

--- banking/main.go
+++ banking/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"fullName"`
-	City    string `json:"city"`
-	Zipcode string `json:"zipcode"`
+	Name    string `json:"fullName" xml:"fullName"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zipcode" xml:"zipcode"`
 }
 
 func main() {
@@ -31,6 +32,12 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{"A", "CityA", "1"},
 		{"B", "CityB", "2"},
 	}
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
