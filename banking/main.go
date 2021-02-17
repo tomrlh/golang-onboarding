8301f1e684b2fff8326eaab060020cc1cff0b035
Thiago Medeiros <si-thiago@hotmail.com>
2021-02-16 23:19:45 -0300

    Second route with custom field names

--- banking/main.go
+++ banking/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/greet",
-		func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "Hello World!") })
+type Customer struct {
+	Name    string `json:"fullName"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+}
 
-	http.ListenAndServe("localhost:8000", nil)
+func main() {
+	// routes
+	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
+
+	// starting server
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"A", "CityA", "1"},
+		{"B", "CityB", "2"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
